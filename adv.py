ba99a5e2088f508ca51ae605604f19f70da57600
wchamber01 <williamchambers@gmail.com>
2020-05-15 14:26:36 -0400

    almost finished but getting an error

--- adv.py
+++ adv.py
@@ -11,10 +11,10 @@ world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-map_file = "maps/test_cross.txt"
+# map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph = literal_eval(open(map_file, "r").read())
@@ -42,55 +42,80 @@ player = Player(world.starting_room)
 # 12      Go to the reverse direction until you get to a room with an exit to a room that is not visited.
 
 stack = Stack()
-current_path = player.current_room.id
 visited = {}
 traversal_path = []
-exits = []
-previous = None
-previous = set(None)
-print(previous)
 
 
-def reverse(direction):
-    if direction == 'n':
+def reverse(dot):
+    if dot == 'n':
         return 's'
-    if direction == 's':
+    if dot == 's':
         return 'n'
-    if direction == 'w':
+    if dot == 'w':
         return 'e'
-    if direction == 'e':
+    if dot == 'e':
         return 'w'
 
 
-def mark_exits(current_path, path_exits):
-    for exit in path_exits:
+def mark_exits(current_room, path_exits):
+    # visited[current_room] = {}
+    for x in path_exits:
         # Mark room exits with ?s
-        visited[current_path][exit] = '?'
+        visited[current_room][x] = '?'
 
 
-def traverse(current_path):
-    # Build my stack
-    stack.push(current_path)
-    previous = set()
-    # Get all exits for current room
+def traverse(current_room, exits=None, previous=None, dot=None):
+    current_room = player.current_room.id
     exits = player.current_room.get_exits()
-    if current_path not in visited:
-        mark_exits(current_path)
-    ran_dir = random.choice(list(visited[current_path].keys()))
-    visited[current_path][ran_dir] = player.current_room.get_room_in_direction(
-        ran_dir)
-    # set previous to current before traveling
-    previous = current_path
-    # travel to dir
-    player.travel(ran_dir)
-    # add the move to my path
-    traversal_path.append(ran_dir)
-    visited[current_path][reverse(ran_dir)] = previous
-    print(visited)
-    print('trav:', traversal_path)
+    # Build my stack
+    stack.push([current_room, previous, exits, dot])
+
+    # traverse (loop) through all rooms until the end
+    while len(visited) < len(room_graph):
+        # Pop top element from stack and destructure it
+        path = stack.pop()
+        # print(exits)
+        current_room = path[0]
+        previous = path[1]
+        exits = path[2]
+        dot = path[3]
+
+        # Process my element
+        # Check if current room not in visited
+        if current_room not in visited:
+            # Mark current room exits
+            mark_exits(current_room, exits)
+
+        # Am I still here? Have I moved yet? Set current room to previous
+        if dot is not None:
+            visited[current_room][reverse(dot)] = previous
+
+        # Do I have a previous room? Set previous room to current
+        if previous is not None:
+            visited[previous][dot] = current_room
+
+        # Explore all paths in the current room
+        for x in visited[current_room].keys():
+            # If there are unexplored paths in the current room
+            if visited[current_room][x] == '?':
+                # Push my current path to the stack
+                stack.push(path)
+                previous = player.current_room.id
+                player.travel(x)
+                traversal_path.append(x)
+                stack.push(
+                    [x, player.current_room.id, previous, player.current_room.get_exits()])
+                # break the loop
+                break
+
+        # reverse traversal
+        if current_room == player.current_room.id:
+            player.travel(reverse(dot))
+            traversal_path.append(reverse(dot))
 
 
-print(traverse())
+traverse(player.current_room.id)
+
 
 # TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
